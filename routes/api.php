41830619145285e8093d943f72e94be9f41f736f
websturing <websturing.project@gmail.com>
2025-07-04 02:05:07 +0000

    [Feature] update auth routes middleware configuration for improved access control

--- routes/api.php
+++ routes/api.php
@@ -3,11 +3,20 @@
 use App\Http\Controllers\API\AuthController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Request;
+use App\Models\Setting;
 
 
 Route::prefix('auth')
-    ->middleware(['web', 'auth:sanctum'])
+    ->middleware(['web'])
     ->group(function () {
         Route::post('/login', [AuthController::class, 'login']);
         Route::get('/profile', [AuthController::class, 'profile'])->middleware('auth:sanctum');
+    });
+
+
+Route::middleware(['auth:sanctum', 'web'])
+    ->group(function () {
+        Route::get('/meta', function () {
+            return Setting::pluck('value', 'key');
+        });
     });
