daf439340503371e040b3e6da0aae547bfdadfbc
websturing <websturing.project@gmail.com>
2025-07-23 02:41:04 +0000

    [Refactor] Fix key name with snake case

--- app/Http/Resources/UserWithRolesResources.php
+++ app/Http/Resources/UserWithRolesResources.php
@@ -18,7 +18,7 @@ class UserWithRolesResources extends JsonResource
             "id" => $this->id,
             "name" => $this->name,
             "email" => $this->email,
-            "roles" => $this->getRoleNames()
+            "role_names" => $this->getRoleNames()
         ];
     }
 }
