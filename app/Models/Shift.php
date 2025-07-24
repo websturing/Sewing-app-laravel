1cbebd4cee72b1b5a9a3a33c6360b6222661210a
websturing <websturing.project@gmail.com>
2025-07-24 08:39:36 +0000

    [Feature] Fillabl on model and CRUD in Repository

--- app/Models/Shift.php
+++ app/Models/Shift.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Shift extends Model
 {
-    //
+    protected $fillable = ['name', 'start_time', 'end_time', 'tolerance', 'is_night_shift'];
 }
--- app/Repositories/Shift/ShiftRepository.php
+++ app/Repositories/Shift/ShiftRepository.php
@@ -10,4 +10,26 @@ class ShiftRepository implements ShiftRepositoryInterface
     {
         return Shift::all();
     }
+
+    public function create(array $shitfData)
+    {
+        return Shift::create($shitfData);
+    }
+
+    public function update(int $id, array $data)
+    {
+        $shift = Shift::findOrFail($id);
+        $shift->update($data);
+        return $shift;
+    }
+
+
+    public function delete(int $id)
+    {
+        $Shift = Shift::find($id);
+        if ($Shift) {
+            return $Shift->delete();
+        }
+        return false;
+    }
 }
