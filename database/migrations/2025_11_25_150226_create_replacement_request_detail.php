ca35eec6c9f02612aea3616ac29ddedfe387ebb8
websturing <websturing.project@gmail.com>
2025-11-27 13:03:44 +0700

    ✨ feat(database): add laying_planning_id to replacement request detail table
    - add new column 'laying_planning_id' to the replacement_request_detail table schema definition

--- database/migrations/2025_11_25_150226_create_replacement_request_detail.php
+++ database/migrations/2025_11_25_150226_create_replacement_request_detail.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->string('color', 255);
             $table->integer('pcs');
             $table->bigInteger('line_id');
+            $table->bigInteger('laying_planning_id');
             $table->string('description', 255);
             $table->timestamps();
         });
